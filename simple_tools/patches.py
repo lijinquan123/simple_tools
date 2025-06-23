15ade8c8cb05d0f72a30f0d8da1470dc7384792f
lijinquan123 <3291742120@qq.com>
2025-06-23 16:45:47 +0800

    feat(simple_tools): 更新版本号并添加 subprocess 补丁
    - 将 simple_tools 版本号从 2.0.30 升级到 2.0.31
    - 在 patches.py 中添加 patch_popen 函数，用于自动填充 subprocess.Popen 的 encoding 参数

--- simple_tools/patches.py
+++ simple_tools/patches.py
@@ -3,8 +3,16 @@
 # @Time        : 2023/3/10 16:59
 # @Version     : Python 3.6.4
 import functools
+import subprocess
 
 import requests
+
+
+def patch_popen(encoding: str = "utf-8"):
+    """
+    自动填充encoding参数, 默认设置"utf-8", 防止报编码错误, 如execjs.eval
+    """
+    subprocess.Popen = functools.partial(subprocess.Popen, encoding=encoding)
 
 
 def patch_requests(timeout: (float, tuple) = 60):
